62f28d30a069135f9c48678507203958adfc334f
Igor Pashev <pashev.igor@gmail.com>
2016-09-29 13:51:44 +0300

    Moved everything into ./modules

--- default.nix
+++ default.nix
@@ -1,16 +1,4 @@
-{ lib, ... }:
-
-let
-  all = lib.filterAttrs
-    ( n: _: n != "default.nix"
-           && ! lib.hasPrefix "." n
-           && ! lib.hasPrefix "LICENSE" n
-           && ! lib.hasPrefix "README" n
-           && ! lib.hasPrefix "ChangeLog" n
-           && ! lib.hasPrefix "TODO" n
-    ) (builtins.readDir ./.);
-
-in {
-  imports = map (p: ./. + "/${p}") ( builtins.attrNames all );
+{
+  imports = [ ./modules ];
 }
 
