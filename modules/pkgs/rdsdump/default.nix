f7bb9e36ae9b82c488c129bd012bbb78a5c813ef
Igor Pashev <pashev.igor@gmail.com>
2017-03-10 05:40:20 +0300

    rdsdump: wrap for mysql client binaries

--- modules/pkgs/rdsdump/default.nix
+++ modules/pkgs/rdsdump/default.nix
@@ -1,14 +1,17 @@
-{ stdenv, bash, ... }:
+{ stdenv, bash, mysql, makeWrapper }:
 
 stdenv.mkDerivation {
   name = "rdsdump";
   buildInputs = [ bash ];
   phases = [ "installPhase" ];
+  nativeBuildInputs = [ makeWrapper ];
   installPhase = ''
     mkdir -p $out/bin
     cp -a ${./rdsdump.bash} $out/bin/rdsdump
     chmod +x $out/bin/rdsdump
     patchShebangs $out/bin/rdsdump
+    wrapProgram "$out/bin/rdsdump" \
+      --prefix PATH : '${mysql.client.bin}/bin'
   '';
 }
 
