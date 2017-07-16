1b575cc657327ef7372dfe848ec031c6946007c4
Igor Pashev <pashev.igor@gmail.com>
2017-07-16 20:19:47 +0300

    nodejs-sass: read package version from main.json

--- modules/pkgs/nodejs-sass/default.nix
+++ modules/pkgs/nodejs-sass/default.nix
@@ -2,17 +2,30 @@
 
 let
 
-  version = "4.5.3";
+  inherit (builtins)
+    attrNames
+    fromJSON
+    head
+    readFile
+    ;
+
+  packages = fromJSON (readFile ./main.json);
+  package = head packages;
+
+  name = head (attrNames package);
+  version = package.${name};
 
   main = (import ./main.nix {
     inherit pkgs;
     inherit (pkgs) nodejs;
     inherit (stdenv) system;
-  })."node-sass-${version}";
+  })."${name}-${version}";
+
 
 in
 pkgs.runCommand "nodejs-sass-${version}" {}
 ''
   mkdir -p $out/bin
   ln -s ${main}/lib/node_modules/node-sass/bin/node-sass $out/bin/node-sass
+  test -x $out/bin/node-sass
 ''
