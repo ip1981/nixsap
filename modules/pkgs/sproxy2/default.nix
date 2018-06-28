98b9d61bc33b42699b70830fa5a77d8dfac3b30b
Igor Pashev <pashev.igor@gmail.com>
2018-06-28 17:05:27 +0300

    sproxy2: drop overrides
    Fails to build with recent nixpkgs.

--- modules/pkgs/sproxy2/default.nix
+++ modules/pkgs/sproxy2/default.nix
@@ -1,16 +1,4 @@
 { haskellPackages }:
 
-/*
-  XXX: Sproxy2 need some libraries missed in nixpkgs 16.09
-*/
-
-let myHaskellPkgs = haskellPackages.override {
-  overrides = self: super: {
-    http-client     = self.callPackage ./http-client.nix {};
-    http-client-tls = self.callPackage ./http-client-tls.nix {};
-    http-conduit    = self.callPackage ./http-conduit.nix {};
-  };
-};
-
-in myHaskellPkgs.callPackage ./main.nix { }
+haskellPackages.callPackage ./main.nix { }
 
