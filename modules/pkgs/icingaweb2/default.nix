8d908c378c1eaae71dd6fdef2f5af64f9cce9ed4
Igor Pashev <pashev.igor@gmail.com>
2018-07-09 16:53:07 +0300

    icingaweb2: 2.5.1 -> 2.5.3

--- modules/pkgs/icingaweb2/default.nix
+++ modules/pkgs/icingaweb2/default.nix
@@ -3,12 +3,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "2.5.1";
+  version = "2.5.3";
   name = "icingaweb2-${version}";
 
   src = fetchurl {
     url = "https://github.com/Icinga/icingaweb2/archive/v${version}.tar.gz";
-    sha256 = "047s43amqj0i4k4xfac3n0784yvzphv3b9kirr4wycvn9pcz06d4";
+    sha256 = "14k5rn09v2ww71x6d8p9rh980nwsmwan2gff0b82dvcmb02576fs";
   };
 
   patches = [
